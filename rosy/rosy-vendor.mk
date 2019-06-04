b41356e4c2a6bf76a4a531bf9efa9e7d72c5483f
muralivijay <muralidharan9845@gmail.com>
2019-06-05 05:12:26 +0530

    rosy: Drop MTP_Global acdbdata blobs

--- rosy/rosy-vendor.mk
+++ rosy/rosy-vendor.mk
@@ -31,20 +31,6 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP/msm8953-tasha-snd-card/MTP_WCD9335_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP/msm8953-tasha-snd-card/MTP_WCD9335_Speaker_cal.acdb \
     vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP/msm8953-tasha-snd-card/MTP_WCD9335_workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP/msm8953-tasha-snd-card/MTP_WCD9335_workspaceFile.qwsp \
     vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP/MTP_workspaceFile.qwsp:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP/MTP_workspaceFile.qwsp \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/MTP_Global_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/MTP_Global_Bluetooth_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/MTP_Global_General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/MTP_Global_General_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/MTP_Global_Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/MTP_Global_Global_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/MTP_Global_Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/MTP_Global_Handset_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/MTP_Global_Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/MTP_Global_Hdmi_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/MTP_Global_Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/MTP_Global_Headset_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/MTP_Global_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/MTP_Global_Speaker_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Bluetooth_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Bluetooth_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_General_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_General_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Global_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Global_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Handset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Handset_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Hdmi_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Hdmi_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Headset_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Headset_cal.acdb \
-    vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Speaker_cal.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/MTP_Global/msm8953-tasha-snd-card/MTP_Global_WCD9335_Speaker_cal.acdb \
     vendor/xiaomi/rosy/proprietary/vendor/etc/acdbdata/adsp_avs_config.acdb:$(TARGET_COPY_OUT_VENDOR)/etc/acdbdata/adsp_avs_config.acdb \
     vendor/xiaomi/rosy/proprietary/vendor/etc/camera/camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camera_config.xml \
     vendor/xiaomi/rosy/proprietary/vendor/etc/camera/csidtg_camera.xml:$(TARGET_COPY_OUT_VENDOR)/etc/camera/csidtg_camera.xml \
